66a3caacf9585682b2e3c93a9339debfaed32fcd
Fides <fidesplay@gmail.com>
2025-07-09 19:42:50 +0200

    permisos v1

--- src/app/bbdd.service.ts
+++ src/app/bbdd.service.ts
@@ -1,23 +1,21 @@
 import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
 import { Observable } from 'rxjs';
+import { environment } from '../environments/environment'; // 👈 Importar entorno
 
 @Injectable({
   providedIn: 'root'
 })
 export class BBDD_service {
-  private baseUrl = 'http://localhost:3000'; // URL del backend
-
+  private baseUrl = environment.apiUrl; // ✅ Usar URL dinámica
 
   constructor(private http: HttpClient) {}
 
-  // Registrar un nuevo usuario
-  registerUser(data: { name_user: string; email: string; password: string }): Observable<any> {
-    console.log('Datos enviados desde el servicio al backend:', data);
-    return this.http.post(`${this.baseUrl}/register`, data);
+// registerUser(data: { name_user: string; email: string; password: string }): Observable<any> {
+//    return this.http.post(`${this.baseUrl}/register`, data);
+//  }
+
+  loginUser(data: any): Observable<any> {
+    return this.http.post(`${this.baseUrl}/login`, data);
   }
-    // Método para iniciar sesión
-    loginUser(data: any): Observable<any> {
-      return this.http.post(`${this.baseUrl}/login`, data);
-    }
 }
