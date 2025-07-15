3526169dcbabeb48fbf5e860116b8041fdca79b5
Fides <fidesplay@gmail.com>
2025-07-15 13:59:34 +0200

    add getServerSideRenderingConfig

--- src/app/app.config.server.ts
+++ src/app/app.config.server.ts
@@ -12,3 +12,9 @@ const serverConfig: ApplicationConfig = {
 };
 
 export const config = mergeApplicationConfig(appConfig, serverConfig);
+
+export function getServerSideRenderingConfig() {
+  return {
+    routes: serverRoutes
+  };
+}
