f4d4f9940b75f93930a34cba5b4f7c8ea4e7aa61
Fides <fidesplay@gmail.com>
2025-07-15 14:07:00 +0200

    fix fix fix

--- src/app/app.routes.server.ts
+++ src/app/app.routes.server.ts
@@ -1,8 +1,15 @@
 import { RenderMode, ServerRoute } from '@angular/ssr';
 
 export const serverRoutes: ServerRoute[] = [
-  {
-    path: '**',
-    renderMode: RenderMode.Prerender
-  }
+  { path: '', renderMode: RenderMode.Prerender },
+  { path: 'register', renderMode: RenderMode.Prerender },
+  { path: 'login', renderMode: RenderMode.Prerender },
+  { path: 'articulos', renderMode: RenderMode.Prerender },
+  { path: 'libros', renderMode: RenderMode.Prerender },
+  { path: 'biografia', renderMode: RenderMode.Prerender },
+  { path: 'nuevo-articulo', renderMode: RenderMode.Prerender },
+  { path: 'nuevo-libro', renderMode: RenderMode.Prerender }
+
 ];
+
+
