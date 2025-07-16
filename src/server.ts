f47f899605c1e9f87f228cd7e428e203a93ae50a
Fides <fidesplay@gmail.com>
2025-07-16 19:35:55 +0200

    fix

--- src/server.ts
+++ src/server.ts
@@ -7,29 +7,16 @@ import {
 import express from 'express';
 import { dirname, resolve } from 'node:path';
 import { fileURLToPath } from 'node:url';
-import { getPrerenderRoutes, getPrerenderParams } from './app/prerender.config.js';
+import { serverRoutes } from './app/server.routes.js'; // 👈 nuevo archivo
 
 const serverDistFolder = dirname(fileURLToPath(import.meta.url));
 const browserDistFolder = resolve(serverDistFolder, '../browser');
 
 const app = express();
-const angularApp = new AngularNodeAppEngine();
 
-/**
- * Example Express Rest API endpoints can be defined here.
- * Uncomment and define endpoints as necessary.
- *
- * Example:
- * ```ts
- * app.get('/api/**', (req, res) => {
- *   // Handle API request
- * });
- * ```
- */
+// ✅ nuevo motor con rutas manuales
+const angularApp = new AngularNodeAppEngine;
 
-/**
- * Serve static files from /browser
- */
 
 app.use(
   express.static(browserDistFolder, {
@@ -39,30 +26,20 @@ app.use(
   }),
 );
 
-/**
- * Handle all other requests by rendering the Angular application.
- */
 app.use('/**', (req, res, next) => {
   angularApp
     .handle(req)
     .then((response) =>
-      response ? writeResponseToNodeResponse(response, res) : next(),
+      response ? writeResponseToNodeResponse(response, res) : next()
     )
     .catch(next);
 });
 
-/**
- * Start the server if this module is the main entry point.
- * The server listens on the port defined by the `PORT` environment variable, or defaults to 4000.
- */
 if (isMainModule(import.meta.url)) {
   const port = process.env['PORT'] || 4000;
   app.listen(port, () => {
-    console.log(`Node Express server listening on http://localhost:${port}`);
+    console.log(`✅ SSR activo en http://localhost:${port}`);
   });
 }
 
-/**
- * The request handler used by the Angular CLI (dev-server and during build).
- */
 export const reqHandler = createNodeRequestHandler(app);
