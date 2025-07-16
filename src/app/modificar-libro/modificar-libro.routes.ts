f8a8b2b3e309b2021a79c761b357b34147af9b95
Fides <fidesplay@gmail.com>
2025-07-16 19:51:38 +0200

    add netlify.toml

--- src/app/modificar-libro/modificar-libro.routes.ts
+++ src/app/modificar-libro/modificar-libro.routes.ts
@@ -1,6 +1,7 @@
 import { Routes } from '@angular/router';
 import { ModificarLibroComponent } from './modificar-libro.component';
 const isPrerender = process.env['PRERENDER'] === 'true';
+
 console.log('📦 Cargando rutas de modificar-libro');
 export const routes: Routes = [
   isPrerender
