3d5c82f00f17ac8f2ecb9ef676b0637ae0f21fa0
Fides <fidesplay@gmail.com>
2025-07-16 18:32:17 +0200

    ddsfs

--- src/main.server.ts
+++ src/main.server.ts
@@ -2,6 +2,9 @@ import { bootstrapApplication } from '@angular/platform-browser';
 import { AppComponent } from './app/app.component';
 import { config } from './app/app.config.server';
 
+// ⬅️ ✅ Exportar las funciones de prerender
+export { getPrerenderParams, getPrerenderRoutes, prerenderOptions } from './app/prerender.config';
+
 const bootstrap = () => bootstrapApplication(AppComponent, config);
 
 export default bootstrap;
